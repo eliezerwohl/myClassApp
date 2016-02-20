681a5d458c606621488689c00d3477547b3d13c3
eliezerwohl <eliezerwohl@gmail.com>
2016-02-20 17:16:17 -0500

    added sequilize and database for users, with all correct fields

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/students', routes);
 app.use('/students', routes);
 
 // sequelize.sync().then(function(){
-  app.listen(PORT, function() {
-    console.log("Listening on port %s", PORT);
-  })
+//   app.listen(PORT, function() {
+//     console.log("Listening on port %s", PORT);
+//   })
 // });
